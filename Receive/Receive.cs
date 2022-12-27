2568e4459572b179947c58f73dd01e8a95916681
Robert Macias <Robert.Macias@trialcard.com>
2022-12-27 11:31:36 -0500

    feat: Adding a sender (publisher) and the receiver (subscriber).

--- Receive/Receive.cs
+++ Receive/Receive.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+public class Receive {
+    public static void Main() {
+        var factory = new ConnectionFactory() {
+            HostName = "localhost",
+            Port = 5672
+        };
+
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel()) {
+            channel.QueueDeclare( 
+                queue: "hello-queue",
+                durable: false, 
+                exclusive: false,
+                autoDelete: false, 
+                arguments: null
+            );
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) => {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($" [x] Received {message}");
+            };
+
+            channel.BasicConsume(
+                queue: "hello-queue",  // routingKey
+                autoAck: true,
+                consumer: consumer
+            );
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
--- Send/Send.cs
+++ Send/Send.cs
@@ -1,3 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadLine();
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+public class Send {
+    public static void Main () {
+        var factory = new ConnectionFactory() {
+            HostName = "localhost", 
+            Port = 5672
+        };
+
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel()) {
+            channel.QueueDeclare(
+                queue: "hello-queue",
+                durable: false, 
+                exclusive: false,
+                autoDelete: false,
+                arguments: null
+            );
+
+            string message = "Mensaje: tururururu!";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(
+                exchange: "",
+                routingKey: "hello-queue",
+                basicProperties: null,
+                body: body
+            );
+
+            Console.WriteLine($"[x] Sent {message}");
+        }
+
+        Console.WriteLine("Press [enter] to exit.");
+        Console.ReadLine();
+    }
+}
